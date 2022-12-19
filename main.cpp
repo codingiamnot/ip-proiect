f4b062f05713a55a1bf172e9094eccb1cb88c403
MihaelaHudisteanu <mihaela.hudisteanu13@gmail.com>
2022-12-19 11:52:06 +0200

    grafica

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <graphics.h>
 #include <winbgim.h>
+#include <ctime>
 
 using namespace std;
 
@@ -31,9 +32,9 @@ float procentDeOcupareEcran = 0.75;
 
 float rotatieX = 0, rotatieY =0;
 
+bool animOn = true, toggleAnimation = true;
 
-
-bool animOn = true;
+int errorScreen=0, mainScreeen;
 
 void citire(string &address)
 {
@@ -367,7 +368,7 @@ void mainLoop()
         muchii.push_back({x, y});
     }
 
-    initwindow(screenWidth, screenHeight);
+    ///initwindow(screenWidth , screenHeight);
 
 
     while(true)
@@ -375,16 +376,282 @@ void mainLoop()
         if(animOn)
             roteste(0.1, 0.1);
 
-        cleardevice();
+        ///cleardevice();
 
+
+        setcolor(WHITE);
         deseneaza();
+
         this_thread::sleep_for( chrono::milliseconds( 100 / 3 ) );
+
+        setcolor(DARKGRAY);
+        deseneaza();
     }
 }
+
+struct button
+{
+    int x1,y1,x2,y2;
+    string txt;
+
+    button(int xA, int yA, int xB, int yB, const string &text)
+    {
+        txt = text;
+        x1 = xA;
+        x2 = xB;
+        y1 = yA;
+        y2 = yB;
+    }
+
+    void draw()
+    {
+        setcolor(BLACK);
+
+        line(x1, y1, x2, y1);
+        line(x2, y1, x2, y2);
+        line(x2, y2, x1, y2);
+        line(x1, y2, x1, y1);
+
+        setfillstyle(SOLID_FILL, WHITE);
+        floodfill(x1+1,y1+1,BLACK);
+
+        if (!txt.empty())
+        {
+            settextstyle(3, HORIZ_DIR, 1);
+            settextjustify(CENTER_TEXT, CENTER_TEXT);
+            setbkcolor(WHITE);
+            setcolor(BLACK);
+            outtextxy((x1 + x2) / 2, (y1 + y2) / 2 + 5, (char *)txt.c_str());
+            //setcolor(BLACK);
+            //setbkcolor(BLACK);
+        }
+    }
+
+    bool isPressed()
+    {
+        int x=mousex(), y=mousey();
+        if(x>=x1 && x<=x2 && y>=y1 && y<=y2)
+            return true;
+        return false;
+    }
+};
+
+void meniu()  ///nu e util
+{
+    initwindow(screenWidth + 200, screenHeight);
+
+    setcolor(DARKGRAY);
+    line(screenWidth,0,screenWidth,screenHeight);
+    setfillstyle(SOLID_FILL, DARKGRAY);
+    floodfill(screenWidth-1,1,DARKGRAY);
+
+    setfillstyle(SOLID_FILL, LIGHTGRAY);
+    floodfill(screenWidth+1,1,DARKGRAY);
+
+    button b1=button(screenWidth+50,50,screenWidth+150,100,"Draw point");
+    b1.draw();
+
+    button b2=button(screenWidth+50,150,screenWidth+150,200,"Draw edge");
+    b2.draw();
+
+    button b3=button(screenWidth+30,screenHeight-100,screenWidth+170,screenHeight-50,"Toggle animation");
+    b3.draw();
+
+    setcolor(LIGHTGRAY);
+    mainLoop();
+
+    getch();
+    closegraph();
+}
+
+void drawError(const string &text1, const string &text2)
+{
+    errorScreen = initwindow(300, 200, "", 400, 250, false, false);
+    settextstyle(3, HORIZ_DIR, 3);
+    settextjustify(CENTER_TEXT, CENTER_TEXT);
+    setbkcolor(BLACK);
+    setcolor(WHITE);
+    outtextxy(150, 75, (char *)text1.c_str());
+    outtextxy(150, 125, (char *)text2.c_str());
+
+    setcurrentwindow(mainScreeen);
+    //getch();
+    ///closegraph(errorScreen);
+    //outtextxy(screenWidth + 100, screenHeight - 90 + 5, (char *)text1.c_str());
+    //outtextxy(screenWidth + 100, screenHeight - 70 + 5, (char *)text2.c_str());
+
+}
+
+void closeError()
+{
+    setcurrentwindow(errorScreen);
+    if(ismouseclick(WM_LBUTTONDOWN))
+        clearmouseclick(WM_LBUTTONDOWN) , closegraph(errorScreen) , errorScreen=0;
+    setcurrentwindow(mainScreeen);
+}
+
+void coverError() //nu ma e bun
+{
+    setfillstyle(SOLID_FILL,LIGHTGRAY);
+    bar(screenWidth , screenHeight - 100 , screenWidth + 200 , screenHeight);
+}
+
+void drawPoint2() ///gresit
+{
+    if(toggleAnimation)
+    {
+        drawError("You can't draw ","during animation!\n");
+        return;
+    }
+
+    bool drawn=0;
+    clock_t currTime=0;
+
+    while(!drawn)
+    {
+        if(ismouseclick(WM_LBUTTONDOWN))
+        {
+            clearmouseclick(WM_LBUTTONDOWN);
+            int x=mousex(), y=mousey();
+            if(x>=screenWidth && x<=screenWidth+200 && y>=0 && y<=screenHeight)
+                drawError("You can't draw ","a point there!\n") , currTime=clock();
+            else
+            {
+                drawn = 1;
+                coverError();
+                ///functie de desenare
+            }
+        }
+        if((float)clock()/CLOCKS_PER_SEC - (float)currTime/CLOCKS_PER_SEC >= 1.5)
+                coverError();
+    }
+}
+
+void drawPoint()
+{
+    if(toggleAnimation)
+    {
+        drawError("You can't draw ","during animation!\n");
+        return;
+    }
+
+    bool drawn=0;
+
+    while(!drawn)
+    {
+        if(errorScreen)
+            closeError();
+
+        if(ismouseclick(WM_LBUTTONDOWN))
+        {
+            clearmouseclick(WM_LBUTTONDOWN);
+            int x=mousex(), y=mousey();
+
+            if(x>=screenWidth && x<=screenWidth+200 && y>=0 && y<=screenHeight)
+                drawError("You can't draw ","a point there!\n");
+            else
+            {
+                drawn = 1;
+                ///functie de desenare
+            }
+        }
+
+    }
+}
+
+void drawEdge()
+{
+     if(toggleAnimation)
+    {
+        drawError("You can't draw ","during animation!\n");
+        return;
+    }
+
+    ///
+}
+
+void program()
+{
+    mainScreeen = initwindow(screenWidth + 200, screenHeight, "3D Visualizer", 200, 25);
+
+    setcolor(DARKGRAY);
+    line(screenWidth,0,screenWidth,screenHeight);
+    setfillstyle(SOLID_FILL, DARKGRAY);
+    floodfill(screenWidth-1,1,DARKGRAY);
+
+    setfillstyle(SOLID_FILL, LIGHTGRAY);
+    floodfill(screenWidth+1,1,DARKGRAY);
+
+    button b1=button(screenWidth+50,50,screenWidth+150,100,"Draw point");
+    b1.draw();
+
+    button b2=button(screenWidth+50,150,screenWidth+150,200,"Draw edge");
+    b2.draw();
+
+    button b3=button(screenWidth+30,250,screenWidth+170,300,"Toggle animation");
+    b3.draw();
+
+    while(true)
+    {
+        if(toggleAnimation)
+        {
+            if(animOn)
+                roteste(0.1, 0.1);
+            setcolor(WHITE);
+            deseneaza();
+        }
+
+
+        if(errorScreen)
+            closeError();
+
+        if(ismouseclick(WM_LBUTTONDOWN))
+        {
+            clearmouseclick(WM_LBUTTONDOWN);
+
+            ///vad in ce caz ma situez
+            if(b1.isPressed())
+                drawPoint();
+            else if(b2.isPressed())
+                drawEdge();
+            else if(b3.isPressed())
+                toggleAnimation=1-toggleAnimation;
+        }
+
+        this_thread::sleep_for( chrono::milliseconds( 100 / 2 ) );
+
+        if(toggleAnimation)
+        {
+            setcolor(DARKGRAY);
+            deseneaza();
+        }
+    }
+
+    getch();
+    closegraph();
+}
+
 
 int main()
 {
     //test();
-    mainLoop();
+    //mainLoop();
+    //meniu();
+    ifstream fin("in.txt");
+    int x,y,z;
+    fin>>n>>m;
+    for(int i=1 ; i<=n ; i++)
+    {
+        fin>>x>>y>>z;
+        puncte.push_back(punct(x, y, z));
+    }
+
+    for(int i=1 ; i<=m ; i++)
+    {
+        fin>>x>>y;
+        muchii.push_back({x, y});
+    }
+
+    program();
     return 0;
 }
